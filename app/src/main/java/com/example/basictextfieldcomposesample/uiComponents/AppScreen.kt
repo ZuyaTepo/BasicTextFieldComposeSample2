bb7131746291368c25816894cc63610a9769e194
ZuyaTepo <mimunesato@gmail.com>
2023-10-03 15:43:49 +0900

    add Column

--- app/src/main/java/com/example/basictextfieldcomposesample/uiComponents/AppScreen.kt
+++ app/src/main/java/com/example/basictextfieldcomposesample/uiComponents/AppScreen.kt
@@ -1,9 +1,12 @@
 package com.example.basictextfieldcomposesample.uiComponents
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun AppScreen() {
-    Text(text = "ずやてっぽ")
+    Column {
+        Text(text = "ずやてっぽ")
+    }
 }
