726cb15d306c519debd47afa8d2f1934df4c1d0d
ZuyaTepo <mimunesato@gmail.com>
2023-10-03 15:23:01 +0900

    add AppScreen

--- app/src/main/java/com/example/basictextfieldcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/basictextfieldcomposesample/MainActivity.kt
@@ -11,18 +11,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basictextfieldcomposesample.ui.theme.BasicTextFieldComposeSampleTheme
+import com.example.basictextfieldcomposesample.uiComponents.AppScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicTextFieldComposeSampleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    AppScreen()
                 }
             }
         }
---
+++ app/src/main/java/com/example/basictextfieldcomposesample/uiComponents/AppScreen.kt
@@ -0,0 +1,9 @@
+package com.example.basictextfieldcomposesample.uiComponents
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppScreen() {
+    Text(text = "ずやてっぽ")
+}
